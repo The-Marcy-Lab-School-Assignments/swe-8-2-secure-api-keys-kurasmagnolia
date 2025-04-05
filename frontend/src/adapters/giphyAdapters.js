7b3a413e5965fbe782eca943cf93cf57d0dd6fb9
kurasmagnolia <kingmmcleod@outlook.com>
2025-04-05 12:38:01 -0400

    added api key to giphyAdapters

--- frontend/src/adapters/giphyAdapters.js
+++ frontend/src/adapters/giphyAdapters.js
@@ -1,6 +1,6 @@
 import { handleFetch } from './handleFetch.js'
 
-const API_KEY = 'How can I include this without exposing it to the public?'
+const API_KEY = 'kcs1s7c89aF0UOKZ3ojJtVobnKiFBKni'
 
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
